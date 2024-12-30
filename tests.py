6590145bfabb5bc79de93316fdf239893d0d6833
swathi-belva <swathi@belva.ai>
2024-12-30 13:01:23 -0800

    commit

--- tests.py
+++ tests.py
@@ -1,5 +1,3 @@
-
-
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
@@ -17,9 +15,8 @@ class CustomUserTests(TestCase):
         self.assertTrue(user.is_active)
         self.assertFalse(user.is_staff)
         self.assertFalse(user.is_superuser)
-        
-        
-    def test_create_superuser(self):    
+
+    def test_create_superuser(self):
         User = get_user_model()
         admin_user = User.objects.create_superuser(
             username='superadmin',
@@ -31,23 +28,19 @@ class CustomUserTests(TestCase):
         self.assertTrue(admin_user.is_active)
         self.assertTrue(admin_user.is_staff)
         self.assertTrue(admin_user.is_superuser)
-        
-        
-        
-"""from django.urls import reverse # new
-#from .forms import CustomUserCreationForm # new#
-class SignupPageTests(TestCase): # new
+
+
+class SignupPageTests(TestCase):
     def setUp(self):
         url = reverse('signup')
         self.response = self.client.get(url)
-        
+
     def test_signup_template(self):
         self.assertEqual(self.response.status_code, 200)
         self.assertTemplateUsed(self.response, 'signup.html')
         self.assertContains(self.response, 'Sign Up')
         self.assertNotContains(
-        self.response, 'Hi there! I should not be on the page.')
-        
-    def test_signup_form(self): # new
+            self.response, 'Hi there! I should not be on the page.')
+
+    def test_signup_form(self):
         form = self.response.context.get('form')
-        #self.assertIsInstance(form, CustomUserCreationForm)"""
